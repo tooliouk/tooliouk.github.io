ebea1d4660fc1749bf7f30918a52ee5968255a6d
Tooliouk <abdulalhassan044@gmail.com>
2025-04-25 19:33:45 +0100

    drawing board (doesn't work, unfinished)

--- scripts.js
+++ scripts.js
@@ -206,3 +206,60 @@ function copyText(text) {
 
   alert("Text Copied!");
 }
+
+// Drawing Board
+
+const canvas = document.getElementById("whiteboard");
+const context = canvas.getContext("2d");
+
+let drawing = false;
+
+canvas.addEventListener("mousedown", () => {
+  drawing = true;
+  context.beginPath();
+});
+
+canvas.addEventListener("mouseup", () => {
+  drawing = false;
+  context.closePath();
+});
+
+canvas.addEventListener("mousemove", draw);
+
+function draw(e) {
+  if (!drawing) return;
+
+  context.lineCap = "round";
+
+  context.lineTo(
+    e.clientX - canvas.getBoundingClientRect().left,
+    e.clientY - canvas.getBoundingClientRect().top
+  );
+  context.stroke();
+  context.beginPath();
+  context.moveTo(
+    e.clientX - canvas.getBoundingClientRect().left,
+    e.clientY - canvas.getBoundingClientRect().top
+  );
+}
+
+//clear
+const clearButton = document.getElementById("clear-btn");
+
+clearButton.addEventListener("click", () => {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+//colour picker
+const colorPicker = document.getElementById("color-picker");
+
+colorPicker.addEventListener("input", (e) => {
+  context.strokeStyle = e.target.value;
+});
+
+// brush size
+const brushSizeInput = document.getElementById("brush-size");
+
+brushSizeInput.addEventListener("input", (e) => {
+  context.lineWidth = e.target.value;
+});
